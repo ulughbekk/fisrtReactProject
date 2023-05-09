60db06eab2c4b9dfe8db95bbfb73205612c49d13
Ulugh <ulugh0618@gmail.com>
2023-05-09 23:26:13 +0900

    first commit componentsAndExpenses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem  from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import NewExpenses from './components/NewExpense/NewExpense';
 
 function App() {
+  const expenses = [
+    { 
+      id: 'e1',
+      title: 'Toilet Paper', 
+      amount: 94.67, 
+      date: new Date(2020, 7, 14)
+    },
+    { 
+      id: 'e2',
+      title: 'New TV', 
+      amount: 799.67, 
+      date: new Date(2023, 2, 12)
+    },
+    { 
+      id: 'e3',
+      title: 'Car Insurance', 
+      amount: 294.67, 
+      date: new Date(2021, 2, 28)
+    },
+    { 
+      id: 'e4',
+      title: 'New Desk (Wooden)', 
+      amount: 450, 
+      date: new Date(2021, 5, 25)
+    },
+    
+  ];
   return (
-    <div className="App">
+    <div>
+      <NewExpenses></NewExpenses>
+       <Expenses item={expenses} />
+    </div>
+  );
+}
+
+export default App;
+/*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +55,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */
